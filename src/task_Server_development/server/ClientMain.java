db5617fcaebfa4ecaddc82ebe2fd3779c0468b5a
m_projects@inbox.lv <Galatasaraj01>
2020-09-30 22:37:55 +0300

    One new branch

---
+++ src/task_Server_development/server/ClientMain.java
@@ -0,0 +1,45 @@
+package task_Server_development.server;
+
+import jdk.jshell.execution.Util;
+
+import java.io.*;
+import java.net.*;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class ClientMain {
+
+
+    public static void main(String[] args) throws IOException   {
+while (true)
+{
+        Socket socket = null;
+        Scanner scanner = new Scanner(System.in);
+        try {
+            socket = new Socket("localhost", 4999);
+            try {
+                InputStream inputStream = socket.getInputStream();
+                OutputStream outputStream = socket.getOutputStream();
+
+                String line = scanner.nextLine();
+                outputStream.write(line.getBytes());
+                outputStream.flush();
+
+                byte[] data = new byte[32 * 1024];
+                int readyBytes = inputStream.read(data);
+
+                System.out.println("Server > " + new String(data, 0, readyBytes));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("get wrong");
+            }
+        }
+            finally {
+                Objects.requireNonNull(socket).close();
+            }
+
+    }
+}}
+
+
