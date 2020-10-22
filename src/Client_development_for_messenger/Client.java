da87c9bfea6a2c6c9fc6522cb3183bb587286681
tipka <tipkanet@gmail.com>
2020-10-22 21:32:56 +0300

    Client branch 22.10.2020

--- src/Client_development_for_messenger/Client.java
+++ src/Client_development_for_messenger/Client.java
@@ -10,69 +10,63 @@ public class Client {
     private static Socket clientSocket; //сокет для соединения
     private static BufferedReader consoleReader; // буфер для чтения из консоли
 
-    //private static InputStream in; // поток чтения из сокета
     private static BufferedReader in; // поток чтения из сокета
-    //private static OutputStream out; // поток записи в сокет
     private static BufferedWriter out; // поток записи в сокет
+
+    private boolean login () throws IOException {
+        System.out.println("Enter username and password separated by a space: ");
+        String login = consoleReader.readLine();
+        out.write("login " + login + "\n");
+        out.flush();
+        System.out.println("...waiting for server' answer...");
+        String serverAnswer = in.readLine();
+        if (serverAnswer.equalsIgnoreCase("ok login")) {
+            System.out.println("Authorization completed successfully! You can start chatting >>>");
+            return true;
+        }
+        else {
+            System.out.println("Authorization failed. Username or password is not correct >>> quit");
+            return false;
+        }
+    }
+
+    private void chatting () throws IOException {
+        while (!clientSocket.isClosed()) {
+            System.out.print(">>>> ");
+            String msg = consoleReader.readLine();
+            out.write("send " + msg + "\n");
+            out.flush();
+            String serverAnswer = in.readLine();
+            System.out.println(serverAnswer);
+        }
+    }
 
     public static void main(String[] args) {
 
+        Client client = new Client();
 
+        consoleReader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Enter the name for authorization: ");
-
-        // Authorization
         try {
-            consoleReader = new BufferedReader(new InputStreamReader(System.in));
             clientSocket = new Socket(HOST, PORT);
             out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            String userName = consoleReader.readLine();
-            out.write(userName + "\n");
-            out.flush();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        String serverAnswer;
-        try {
-            System.out.println("waiting for server' answer...");
-            serverAnswer = in.readLine();
-            if (serverAnswer.equalsIgnoreCase("ok login")) System.out.println("Connection is ready >>>");
-            else
-                if (serverAnswer.equalsIgnoreCase("Login error")) System.out.println("Connection is not ready >>>");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-
-
-        //Chatting
-        String msg = "";
-        try {
-            // while (!clientSocket.isClosed())
-            while (!msg.equalsIgnoreCase("quit")) {
-                System.out.print(">>>> ");
-                msg = consoleReader.readLine();
-                out.write(msg + "\n");
-                out.flush();
+            if (client.login()) {
+                client.chatting();
             }
         } catch (IOException e) {
-                e.printStackTrace();
+            e.printStackTrace();
         } finally {
             try {
+                out.write("quit\n");
+                out.flush();
                 clientSocket.close();
                 in.close();
                 out.close();
             } catch (IOException e) {
-                    e.printStackTrace();
+                e.printStackTrace();
             }
         }
-
     }
-
 }
